337fe86e268ccfc7ebd36fb14500ccd242036700
SlavaNik16 <nikolaevvacheslav2710@gmail.com>
2024-01-29 01:03:02 +0300

    Добавьте файлы проекта.

---
+++ PopryzhenokTest.Contexty/DB/ApplicationContextDB.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Data.Entity;
+
+
+namespace PopryzhenokTest.DB
+{
+
+    public class ApplicationContextDB : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationContextDB() : base("DefaultConnection")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("TUsers");
+            modelBuilder.Entity<User>().HasKey(x => x.Id);
+        }
+
+    }			
+
+}
---
+++ PopryzhenokTest.Contexty/Models/Users.cs
@@ -0,0 +1,63 @@
+﻿namespace PopryzhenokTest
+{
+    public class User
+	{
+
+		///<summary>
+		/// Идентификатор
+		///</summary>
+		public int Id { get; set; }
+
+		///<summary>
+		/// Фамилия
+		///</summary>
+		public string Surname { get; set; }
+
+		///<summary>
+		/// Имя
+		///</summary>
+		public string Name { get; set; }
+
+		///<summary>
+		/// Отчество
+		///</summary>
+		public string Patronymic { get; set; } = null;
+
+		///<summary>
+		/// Логин
+		///</summary>
+		public string Login { get; set; }
+
+		///<summary>
+		/// Пароль
+		///</summary>
+		public string Password { get; set; }
+
+		///<summary>
+		/// Роль
+		///</summary>
+		public RoleTypes RoleType { get; set; }
+	}
+
+	public enum RoleTypes
+	{
+
+		///<summary>
+		/// Клиент
+		///</summary>
+		Client,
+
+		///<summary>
+		/// Менеджер
+		///</summary>
+		Manager,
+
+		///<summary>
+		/// Админ
+		///</summary>
+		Admin,
+	}
+
+
+
+}
